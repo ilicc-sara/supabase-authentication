d47bc34bc1aa803da270928502835a6ba3bf25a4
Sara <sarailic431@gmail.com>
2025-11-29 14:30:01 +0100

    react router and organising pages and components

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,27 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
+import Dashboard from "./components/Dashboard.tsx";
+import SignIn from "./components/SignIn.tsx";
+import SignUp from "./components/SignUp.tsx";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+  },
+  {
+    path: "/signup",
+    element: <SignUp />,
+  },
+  {
+    path: "/signin",
+    element: <SignIn />,
+  },
+  {
+    path: "/dashboard",
+    element: <Dashboard />,
   },
 ]);
 
